5f29713473999e758406ab9a8619e804326d4713
James Scalise <jamescscalise@gmail.com>
2019-07-30 13:57:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,3 +9,6 @@ end
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
+
+introduction("James")
+introduction_with_language("James", "Python")
