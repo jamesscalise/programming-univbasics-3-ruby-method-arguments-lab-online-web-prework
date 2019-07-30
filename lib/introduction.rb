9490ee48edec843b07bbb4654b5343aa000bd32d
James Scalise <jamescscalise@gmail.com>
2019-07-30 13:58:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,12 @@
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction("James")
-introduction_with_language("James", "Python")
