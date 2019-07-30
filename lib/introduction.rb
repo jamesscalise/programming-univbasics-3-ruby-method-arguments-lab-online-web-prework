94004fcb64ff93f2318ed63940865530f147a83e
James Scalise <jamescscalise@gmail.com>
2019-07-30 14:23:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,13 +6,6 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language)
-  if language != nil
-    puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  else
-    puts "Hi, my name is #{name} and I am learning to program in Ruby."
-  end
+def introduction_with_language(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-language = nil
-introduction_with_language_optional("James", language)
